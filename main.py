2d9b6a96672569b7a717f5e8c6ec4bf37327b430
guiraccone <guilherme.raccone@hotmail.com>
2024-02-14 02:36:21 -0300

    feat: clear windows temp folder

--- main.py
+++ main.py
@@ -2,19 +2,27 @@ import os
 import shutil
 import getpass
 
+def clearDirectory(directory):
+    files = os.listdir(directory)
+    for file in files: 
+        filepath = os.path.join(directory, file)
+        try: 
+            if os.path.isfile(filepath):
+                os.unlink(filepath)
+            elif os.path.isdir(filepath):
+                shutil.rmtree(filepath)
+        except Exception as e:
+            print(e)
+
 username = getpass.getuser()
 
-temp_dir = f"C:\\Users\\{username}\\AppData\\Local\\Temp"
+option = int(input("Which temp folder should be cleared ? \n1 - Appdata \n2 - Windows \n0 - Quit \n"))
 
-files = os.listdir(temp_dir)
-for file in files: 
-    filepath = os.path.join(temp_dir, file)
-    try: 
-        if os.path.isfile(filepath):
-            os.unlink(filepath)
-        elif os.path.isdir(filepath):
-            shutil.rmtree(filepath)
-    except Exception as e:
-        print(e)
-        
-print("Temporary files cleared.")
+match option: 
+
+    case 1:
+        clearDirectory(f"C:\\Users\\{username}\\AppData\\Local\\Temp")
+    case 2:
+        clearDirectory(f"C:\\Windows\\Temp")
+    case 0: 
+        exit()
