c52939cb102464daf4e08d6308706e85411cd1f6
guiraccone <guilherme.raccone@hotmail.com>
2024-02-15 14:26:35 -0300

    feat: finally exception

--- main.py
+++ main.py
@@ -4,25 +4,26 @@ import getpass
 
 def clearDirectory(directory):
     files = os.listdir(directory)
-    for file in files: 
+    for file in files:
         filepath = os.path.join(directory, file)
-        try: 
+        try:
             if os.path.isfile(filepath):
                 os.unlink(filepath)
             elif os.path.isdir(filepath):
                 shutil.rmtree(filepath)
         except Exception as e:
             print(e)
+        finally:
+            exit()
 
 username = getpass.getuser()
+option = int(input(
+    "Which temp folder should be cleared ? \n1 - Appdata \n2 - Windows \n0 - Quit \n"))
 
-option = int(input("Which temp folder should be cleared ? \n1 - Appdata \n2 - Windows \n0 - Quit \n"))
-
-match option: 
-
+match option:
     case 1:
         clearDirectory(f"C:\\Users\\{username}\\AppData\\Local\\Temp")
     case 2:
         clearDirectory(f"C:\\Windows\\Temp")
-    case 0: 
+    case 0:
         exit()
